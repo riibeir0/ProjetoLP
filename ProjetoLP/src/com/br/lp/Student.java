f54fc50ad8bc464444feaac666eec12781b67fc0
31462669 <31462669@087856-p33s105.macklabs.br>
2015-05-13 11:25:13 -0300

    inicio projeto

---
+++ ProjetoLP/src/com/br/lp/Student.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.br.lp;
+
+/**
+ *
+ * @author 31462669
+ */
+public class Student {
+    
+}
---
+++ ProjetoLP/src/com/br/lp/University.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.br.lp;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author 31462669
+ */
+public class University {
+
+    private String name;
+    private ArrayList courses;
+    private ArrayList students;
+    private ArrayList registrations;
+
+    public University(String name) {
+        this.name = name;
+    }
+    
+}
