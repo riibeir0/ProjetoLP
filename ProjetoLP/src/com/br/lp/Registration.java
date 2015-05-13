ce655fce278fa56f0fa320af718c232967968080
Gabriel <Gabriel@Gabriel-PC>
2015-05-13 16:44:35 -0300

    metodos

--- ProjetoLP/src/com/br/lp/Registration.java
+++ ProjetoLP/src/com/br/lp/Registration.java
@@ -10,5 +10,49 @@ package com.br.lp;
  * @author Gabriel
  */
 public class Registration {
+    private Student student;
+    private Course course;
+    private int acadYear;
+    private int semester;
+
+    public Registration(Student student, Course course, int acadYear, int semester) {
+        this.student = student;
+        this.course = course;
+        this.acadYear = acadYear;
+        this.semester = semester;
+    }
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+
+    public Course getCourse() {
+        return course;
+    }
+
+    public void setCourse(Course course) {
+        this.course = course;
+    }
+
+    public int getAcadYear() {
+        return acadYear;
+    }
+
+    public void setAcadYear(int acadYear) {
+        this.acadYear = acadYear;
+    }
+
+    public int getSemester() {
+        return semester;
+    }
+
+    public void setSemester(int semester) {
+        this.semester = semester;
+    }
+    
     
 }
--- ProjetoLP/src/com/br/lp/University.java
+++ ProjetoLP/src/com/br/lp/University.java
@@ -14,12 +14,43 @@ import java.util.ArrayList;
 public class University {
 
     private String name;
-    private ArrayList courses;
-    private ArrayList students;
-    private ArrayList registrations;
+    private ArrayList<Course> courses;
+    private ArrayList<Student> students;
+    private ArrayList<Registration> registrations;
 
     public University(String name) {
         this.name = name;
     }
+    
+    public void addCourse(String code, String title, int numCredits, int maxStudents) throws Exception{
+        Course a = new Course(code, title, numCredits, maxStudents);
+        for (Course cursoExistente : courses) { //compara o curso com o existente
+            if(a.getTitle().equals(cursoExistente.getTitle())){
+                throw new Exception("Curso já existe!");
+            } else{
+                courses.add(a);
+            }
+        }
+    } //AVISAR A PROFESSORA DO TIPO DE RETORNO, NÃO FAZ SENTIDO RETORNAR O CURSO QUE ESTÁ SENDO ADICIONADO!!!!111
+    
+    public boolean updateCourse(String code, String title, int numCredits, int maxStudents) throws Exception{
+        boolean flag = false;
+        for (Course course : courses) {
+            if(course.getTitle().equals(title)){
+                course.setCode(code);
+                course.setMaxStudents(maxStudents);
+                course.setNumCredits(numCredits);
+                flag = true;
+            }
+        }
+        if(flag){
+            return true;
+        } else{
+            return false;
+        }
+        
+    } //AVISAR A PROFESSORA DO TIPO DE RETORNO, DEVERIA SER BOOLEAN
+    
+    
     
 }
